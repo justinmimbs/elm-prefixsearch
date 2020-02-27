9017b3a0323ad537125f2e339a66ce0ffeef4044
Justin Mimbs <justin.mimbs@gmail.com>
2020-02-27 11:27:51 -0500

    Add benchmarks for searchAll function

--- bench/Main.elm
+++ bench/Main.elm
@@ -7,20 +7,12 @@ import Types exposing (Index, exampleText, fill)
 
 main : Benchmark.Runner.BenchmarkProgram
 main =
-    Benchmark.Runner.program benchmarks
+    Benchmark.Runner.program <|
+        benchmark_searchAll Types.trieset Types.trielist
 
 
-benchmarks : Benchmark
-benchmarks =
-    Benchmark.describe "prefix search"
-        ([ "a", "am", "ampl", "amplifica", "amplifications" ]
-            |> List.map
-                (compare Types.trieset Types.trielist)
-        )
-
-
-compare : Index a -> Index b -> String -> Benchmark
-compare a b =
+benchmark_search : Index a -> Index b -> Benchmark
+benchmark_search a b =
     let
         x =
             a.empty |> fill exampleText a.insert
@@ -28,9 +20,46 @@ compare a b =
         y =
             b.empty |> fill exampleText b.insert
     in
-    \keyword ->
-        Benchmark.compare keyword
-            a.name
-            (\() -> x |> a.search keyword)
-            b.name
-            (\() -> y |> b.search keyword)
+    Benchmark.describe "search"
+        ([ "a"
+         , "am"
+         , "ampl"
+         , "amplifica"
+         , "amplifications"
+         ]
+            |> List.map
+                (\keyword ->
+                    Benchmark.compare keyword
+                        a.name
+                        (\() -> x |> a.search keyword)
+                        b.name
+                        (\() -> y |> b.search keyword)
+                )
+        )
+
+
+benchmark_searchAll : Index a -> Index b -> Benchmark
+benchmark_searchAll a b =
+    let
+        x =
+            a.empty |> fill exampleText a.insert
+
+        y =
+            b.empty |> fill exampleText b.insert
+    in
+    Benchmark.describe "searchAll"
+        ([ [ "d", "c" ]
+         , [ "de", "co" ]
+         , [ "def", "con" ]
+         , [ "defa", "cons" ]
+         , [ "default", "conscience" ]
+         ]
+            |> List.map
+                (\keywords ->
+                    Benchmark.compare (keywords |> String.join ", ")
+                        a.name
+                        (\() -> x |> a.searchAll keywords)
+                        b.name
+                        (\() -> y |> b.searchAll keywords)
+                )
+        )
